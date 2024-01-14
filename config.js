38ace845817d4c9ed0975fce804a16d9689accf7
UKLUCIFER999 <156609776+UKLUCIFER999@users.noreply.github.com>
2024-01-15 02:01:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['918714258795', 'UK_LUCIFER', true],
+  ['918714258795', 'UK_LUCIFER', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'UK_LUCIFER_ʙᴏᴛ'
+global.premium = 'false'
+global.packname = 'UKLUCIFER' 
+global.author = '@LUCIFER' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
